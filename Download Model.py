ea77d0c52ec93ca372c596ed6b3ed89248d959e2
NSANTRA <neelotpal.santra@gmail.com>
2025-10-22 11:11:06 +0530

    Script for downloading required models

--- Download Model.py
+++ Download Model.py
@@ -1,4 +1,7 @@
+import os
 from sentence_transformers import SentenceTransformer, CrossEncoder
+
+os.makedirs("Models", exist_ok = True)
 
 encoder = SentenceTransformer("sentence-transformers/all-MiniLM-L6-v2")
 encoder.save_pretrained("Models/MiniLM")
